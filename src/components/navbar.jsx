da1a3546edbfbc4b5fa4ba31622b25f6240c1111
christian <christianbiringanine22@gmail.com>
2023-05-23 10:52:39 +0200

    Add the dropdown side effect on add task

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,34 @@
-const NavBar = ({ tasks, onCompleted, onActive, onAll }) => {
+import { useEffect, useRef, useState } from "react";
+
+const NavBar = ({ tasks, handleAdd, onCompleted, onActive, onAll }) => {
   const left = tasks.filter((item) => !item.completed).length;
+  const [dropdown, setDropDown] = useState(false);
+  const ref = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropDown(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [dropdown]);
   return (
     <div className="navbar">
       <div className="nav__left">
-        <span>
+        <span
+          ref={ref}
+          onClick={() => setDropDown((prev) => !prev)}
+          className="add_task-container"
+        >
           <i className="fa fa-plus"></i>Add
+          {dropdown && (
+            <div className="add_task">
+              <input type="text" placeholder="Add New" />
+            </div>
+          )}
         </span>
         <span>
           <i className="fa fa-search"></i>Search
